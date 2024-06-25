2c1f88f8308bde70950ef86324ea4cadb3b474f3
Doktorlich <86869435+Doktorlich@users.noreply.github.com>
2024-06-25 13:02:55 +0300

    first commit

---
+++ src/01-app/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/global.scss";
+
+import Home from "../03-pages/Home/Home";
+function App() {
+    return (
+        <>
+            <Home />
+        </>
+    );
+}
+
+export default App;
---
+++ src/03-pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Card from "../../07-shared/UI/Card/Card";
+
+const Home = () => {
+    return <Card>adsadsads</Card>;
+};
+
+export default Home;
