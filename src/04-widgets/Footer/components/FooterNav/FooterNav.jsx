f9697745647a3e46a380f70b4de8cd459f0863a8
Doktorlich <86869435+Doktorlich@users.noreply.github.com>
2024-06-27 11:02:46 +0300

    Create adaptive layout to footer
     Changes to be committed:
    	modified:   src/04-widgets/Footer/components/ContactInfo/ContactInfo.module.scss
    	modified:   src/04-widgets/Footer/components/FooterNav/FooterNav.jsx
    	modified:   src/04-widgets/Footer/components/FooterNav/FooterNav.module.scss
    	modified:   src/04-widgets/Footer/components/TopFooterBlock/TopFooterBlock.module.scss
    	modified:   src/04-widgets/Footer/components/TopFooterBlock/TopFooterElement/ServicesBlock.module.scss
    	modified:   src/04-widgets/Footer/components/TopFooterBlock/TopFooterElement/SubscribeNewsletter.module.scss

--- src/04-widgets/Footer/components/FooterNav/FooterNav.jsx
+++ src/04-widgets/Footer/components/FooterNav/FooterNav.jsx
@@ -17,36 +17,43 @@ const SOCIAL_MEDIA = [facebook, instagram, twitter, linkedin, union];
 
 const FooterNav = () => {
     return (
-        <div className={styled.nav}>
-            {NAV_LIST.map((item, indexUl) => (
-                <ul key={indexUl} className={styled["nav__list"]}>
-                    <h3 className={styled["nav__title"]}>{item.title}</h3>
-                    {item.listLink.map((link, indexLi) => (
-                        <li key={indexLi} className={styled["nav__list-item"]}>
-                            <a href="/" className={styled["nav__list-link"]}>
-                                {link}
-                            </a>
-                        </li>
+        <>
+            <div className={styled.nav}>
+                <div className={styled["nav__inner"]}>
+                    {NAV_LIST.map((item, indexUl) => (
+                        <ul key={indexUl} className={styled["nav__list"]}>
+                            <h3 className={styled["nav__title"]}>{item.title}</h3>
+                            {item.listLink.map((link, indexLi) => (
+                                <li key={indexLi} className={styled["nav__list-item"]}>
+                                    <a href="/" className={styled["nav__list-link"]}>
+                                        {link}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
                     ))}
-                </ul>
-            ))}
-            <div className={styled.wrapper}>
-                <h3 className={styled["nav__title"]}>Social Media</h3>
-                <ul className={styled["media__list"]}>
-                    {SOCIAL_MEDIA.map((card, index) => (
-                        <li key={index} className={styled["media__list-item"]}>
-                            <a href="/" className={styled["media__list-link"]}>
-                                <img src={card} className={styled["media__list-image"]} />
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-                <div className={styled["block-cards"]}>
-                    <h3 className={styled["nav__title"]}>We accept</h3>
-                    <img src={creditCards} alt="" className={styled["block-cards__cards"]} />
+                    <div className={styled.wrapper}>
+                        <h3 className={styled["nav__title"]}>Social Media</h3>
+                        <ul className={styled["media__list"]}>
+                            {SOCIAL_MEDIA.map((card, index) => (
+                                <li key={index} className={styled["media__list-item"]}>
+                                    <a href="/" className={styled["media__list-link"]}>
+                                        <img src={card} className={styled["media__list-image"]} />
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                        <div className={styled["block-cards"]}>
+                            <h3 className={styled["nav__title"]}>We accept</h3>
+                            <img src={creditCards} alt="" className={styled["block-cards__cards"]} />
+                        </div>
+                    </div>
+                </div>
+                <div className={styled.copyright}>
+                    <p>© 2024 GreenShop. All Rights Reserved.</p>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
