6cb8035e8499ef0b9da0b378ed34bcf61394c779
Doktorlich <86869435+Doktorlich@users.noreply.github.com>
2024-08-02 11:59:18 +0300

    feat(ProductDetails.jsx) Add Skeleton Component to ProductDetails Component

--- src/04-widgets/ProductDetails/ProductDetails.jsx
+++ src/04-widgets/ProductDetails/ProductDetails.jsx
@@ -8,27 +8,75 @@ import ReviewProductDetails from "../../05-features/ReviewProductDetails/ReviewP
 import ProductDescription from "./../../06-entities/ProductDescription/ProductDescription";
 import Tabs from "./../../05-features/Tabs/Tabs";
 import ProductReviews from "./../../06-entities/ProductReviews/ProductReviews";
+import Skeleton from "../../07-shared/UI/Skeleton/Skeleton.jsx";
+
 const ProductDetails = () => {
     const { valueData } = useSelector((state) => state.productView);
     const { status } = useSelector((state) => state.apiProduct);
-    if (status !== "succeeded") {
-        return;
-    }
+
     return (
         <>
             <div className={styled["section-details__inner"]}>
-                <ProductImageDetails valueData={valueData} />
-                <ProductDetailsList valueData={valueData}>
-                    <CountProductDetails valueData={valueData} classNameTransfer={styled["wrapper-transfer-size"]} />
-                    <ReviewProductDetails valueData={valueData} classNameTransfer={styled["wrapper-transfer-review"]} />
-                </ProductDetailsList>
+                {status !== "succeeded" ?
+                    <>
+                        <Skeleton className={styled["skeleton"]}>
+                            <div className={styled["section-details__skeleton"]}>
+                                <ul className={styled["section-details__skeleton-list"]}>
+                                    <li className={styled["section-details__skeleton-item"]}></li>
+                                    <li className={styled["section-details__skeleton-item"]}></li>
+                                    <li className={styled["section-details__skeleton-item"]}></li>
+                                    <li className={styled["section-details__skeleton-item"]}></li>
+                                </ul>
+                                <div className={styled["section-details__skeleton-big-img"]}></div>
+                            </div>
+                        </Skeleton>
+                        <Skeleton className={styled["skeleton"]}>
+                            <div className={styled["section-details__skeleton-details-list"]}>
+
+                                <div className={styled["section-details__skeleton-details-all"]}></div>
+                            </div>
+                        </Skeleton>
+                    </>
+                    :
+                    <>
+                        <ProductImageDetails valueData={valueData} />
+                        <ProductDetailsList valueData={valueData}>
+                            <CountProductDetails
+                                valueData={valueData}
+                                classNameTransfer={styled["wrapper-transfer-size"]}
+                            />
+                            <ReviewProductDetails
+                                valueData={valueData}
+                                classNameTransfer={styled["wrapper-transfer-review"]}
+                            />
+                        </ProductDetailsList>
+                    </>
+                }
             </div>
-            <Tabs>
-                <div className={styled["tabs"]}>
-                    <ProductDescription valueData={valueData} />
-                    <ProductReviews />
-                </div>
-            </Tabs>
+            {status !== "succeeded" ?
+                <Skeleton className={styled["skeleton__bottom"]}>
+                    <div className={styled["skeleton__bottom-section"]}>
+                        <div className={styled["skeleton__tabs"]}>
+                            <p className={styled["skeleton__tabs-item"]}></p>
+                            <p className={styled["skeleton__tabs-item"]}></p>
+                        </div>
+                        <div className={styled["skeleton__description"]}>
+                            <ul className={styled["skeleton__description-list"]}>
+                                {Array.from({ length: 25 }, (_, index) => {
+                                    return <li key={index} className={styled["skeleton__description-item"]}></li>;
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+
+                </Skeleton> :
+                <Tabs>
+                    <div className={styled["tabs"]}>
+                        <ProductDescription valueData={valueData} />
+                        <ProductReviews />
+                    </div>
+                </Tabs>}
+
         </>
     );
 };
