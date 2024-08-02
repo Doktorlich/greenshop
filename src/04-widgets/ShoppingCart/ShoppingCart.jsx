359513293c2f5df2430596eb220fe96a0a0e2bbc
Doktorlich <86869435+Doktorlich@users.noreply.github.com>
2024-08-02 13:38:25 +0300

    feat(ShoppingCart.jsx) Add Skeleton Component to ShoppingCart Component

--- src/04-widgets/ShoppingCart/ShoppingCart.jsx
+++ src/04-widgets/ShoppingCart/ShoppingCart.jsx
@@ -4,6 +4,7 @@ import CartTable from "../../05-features/CartTable/CartTable.jsx";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProductData } from "../../07-shared/api/store/apiSlice.js";
 import CartTotals from "../../05-features/CartTotals/CartTotals.jsx";
+import Skeleton from "../../07-shared/UI/Skeleton/Skeleton.jsx";
 
 const ShoppingCart = () => {
     const { productData, status } = useSelector((state) => state.apiProduct);
@@ -14,13 +15,36 @@ const ShoppingCart = () => {
         dispatch(fetchProductData());
 
     }, []);
-    if (status !== "succeeded") {
-        return "Загрузка...";
-    }
+ 
     return (
         <div className={styled["shopping-cart"]}>
-            <CartTable {...productData} />
-            <CartTotals />
+            {status !== "succeeded" ?
+                <div className={styled["shopping-cart__skeleton"]}>
+                    <Skeleton className={[styled["skeleton"], styled["skeleton-left"]].join(" ")}>
+                        <p className={styled["skeleton-left__title"]}></p>
+                        <ul className={styled["skeleton-left__list"]}>
+                            <li className={styled["skeleton-left__item"]}></li>
+                            <li className={styled["skeleton-left__item"]}></li>
+                            <li className={styled["skeleton-left__item"]}></li>
+                            <li className={styled["skeleton-left__item"]}></li>
+                        </ul>
+                    </Skeleton>
+                    <Skeleton className={[styled["skeleton"], styled["skeleton-right"]].join(" ")}>
+                        <p className={styled["skeleton-right__title"]}></p>
+                        <ul className={styled["skeleton-right__list"]}>
+                            {Array.from({ length: 11 }, (_, index) => {
+                                return <li key={index} className={styled["skeleton-right__item"]}></li>;
+                            })}
+
+                        </ul>
+                    </Skeleton>
+                </div>
+                :
+                <>
+                    <CartTable {...productData} />
+                    <CartTotals />
+                </>
+            }
         </div>
     );
 };
