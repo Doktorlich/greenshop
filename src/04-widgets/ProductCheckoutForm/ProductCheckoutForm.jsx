b5f560528a7684019e53331c7d47bb27c5cd11ac
Doktorlich <86869435+Doktorlich@users.noreply.github.com>
2024-08-02 14:20:56 +0300

    feat(ProductCheckoutForm.jsx) Add Skeleton Component to ProductCheckoutForm Component

--- src/04-widgets/ProductCheckoutForm/ProductCheckoutForm.jsx
+++ src/04-widgets/ProductCheckoutForm/ProductCheckoutForm.jsx
@@ -7,6 +7,7 @@ import { fetchProductData } from "../../07-shared/api/store/apiSlice.js";
 import Modal from "../../07-shared/UI/Modal/Modal.jsx";
 import ModalTrackYourOrder from "../../05-features/ModalTrackYourOrder/ModalTrackYourOrder.jsx";
 import { setActiveModal } from "./provider/productCheckoutFormSlice.js";
+import Skeleton from "../../07-shared/UI/Skeleton/Skeleton.jsx";
 
 
 const ProductCheckoutForm = () => {
@@ -19,13 +20,42 @@ const ProductCheckoutForm = () => {
         dispatch(fetchProductData());
 
     }, []);
-    if (status !== "succeeded") {
-        return "Загрузка...";
-    }
+  
     return (
         <div className={styled["product-checkout"]}>
-            <BillingAddress />
-            <YourOrder {...productData[0]} />
+            {status !== "succeeded" ?
+                <>
+                    <Skeleton className={[styled["skeleton"], styled["skeleton-left"]].join(" ")}>
+                        <ul className={styled["skeleton-left__list"]}>
+                            {Array.from({ length: 11 }, (_, index) => {
+                                return <li key={index} className={styled["skeleton-left__item"]}></li>;
+                            })}
+                        </ul>
+
+                    </Skeleton>
+                    <Skeleton className={[styled["skeleton"], styled["skeleton-right"]].join(" ")}>
+                        <ul className={styled["skeleton-right__product-list"]}>
+                            {Array.from({ length: 4 }, (_, index) => {
+                                return <li key={index} className={styled["skeleton-right__product-item"]}></li>;
+                            })}
+                        </ul>
+                        <ul className={styled["skeleton-right__price-list"]}>
+                            {Array.from({ length: 5 }, (_, index) => {
+                                return <li key={index} className={styled["skeleton-right__price-item"]}></li>;
+                            })}
+                        </ul>
+                        <ul className={styled["skeleton-right__checkbox-list"]}>
+                            {Array.from({ length: 4 }, (_, index) => {
+                                return <li key={index} className={styled["skeleton-right__checkbox-item"]}></li>;
+                            })}
+                        </ul>
+                    </Skeleton>
+                </>
+
+                : <>
+                    <BillingAddress />
+                    <YourOrder {...productData[0]} /></>}
+
             <Modal activeModal={activeModal} setActiveModal={setActiveModal} cNContent={styled["modal-track-order"]}>
                 <ModalTrackYourOrder {...productData[0]} activeModal={activeModal} />
             </Modal>
